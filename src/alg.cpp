8f6bc53dbd6cbf8a2cec4fbadc56e72f00a249ca
egorisan <epodcherednichenko@gmail.com>
2025-04-22 00:22:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2021 NNTU-CS
 
-int countPairs1(int* arr, int len, int value)
-{
+int countPairs1(int* arr, int len, int value){
     int count = 0;
     for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
@@ -12,8 +11,8 @@ int countPairs1(int* arr, int len, int value)
     }
     return count;
 }
-int countPairs2(int* arr, int len, int value)
-{
+
+int countPairs2(int* arr, int len, int value){
     int count = 0;
     int left = 0, right = len - 1;
 
@@ -22,8 +21,7 @@ int countPairs2(int* arr, int len, int value)
         if (sum == value) {
             ++count;
             ++left;
-            --right;
-        }
+            --right;}
         else if (sum < value) {
             ++left;
         }
@@ -52,8 +50,7 @@ int B_search(int* data, int low, int high, int target) {
     return -1;
 }
 
-int countPairs3(int* arr, int size, int targetSum)
-{
+int countPairs3(int* arr, int size, int targetSum){
     int pairCount = 0;
 
     for (int index = 0; index < size; ++index) {
