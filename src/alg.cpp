aad6c68e83d28026e9b749c998c1e1002b709102
egorisan <epodcherednichenko@gmail.com>
2025-04-22 00:31:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,7 @@
 // Copyright 2021 NNTU-CS
 
-int countPairs1(int* arr, int len, int value){
+int countPairs1(int* arr, int len, int value)
+{
     int count = 0;
     for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
@@ -12,7 +13,8 @@ int countPairs1(int* arr, int len, int value){
     return count;
 }
 
-int countPairs2(int* arr, int len, int value){
+int countPairs2(int* arr, int len, int value)
+{
     int count = 0;
     int left = 0, right = len - 1;
 
@@ -20,8 +22,11 @@ int countPairs2(int* arr, int len, int value){
         int sum = arr[left] + arr[right];
         if (sum == value) {
             ++count;
+            while (left < right && arr[left] == arr[left + 1]) ++left;
+            while (left < right && arr[right] == arr[right - 1]) --right;
             ++left;
-            --right;}
+            --right;
+        }
         else if (sum < value) {
             ++left;
         }
@@ -29,38 +34,35 @@ int countPairs2(int* arr, int len, int value){
             --right;
         }
     }
-
     return count;
 }
 
 
-int B_search(int* data, int low, int high, int target) {
+int B_search(int* arr, int low, int high, int target) {
     while (low <= high) {
-        int mid = low + ((high - low) >> 1);
-        if (data[mid] == target) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target) {
             return mid;
         }
-        else if (data[mid] < target) {
+        else if (arr[mid] < target) {
             low = mid + 1;
         }
         else {
-            high = mid - 1; 
+            high = mid - 1;
         }
     }
     return -1;
 }
 
-int countPairs3(int* arr, int size, int targetSum){
-    int pairCount = 0;
-
-    for (int index = 0; index < size; ++index) {
-        int complement = targetSum - arr[index];
-        if (complement >= arr[index]) {
-            if (B_search(arr, index + 1, size - 1, complement) != -1) {
-                ++pairCount;
+int countPairs3(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        int complement = value - arr[i];
+        if (complement >= arr[i]) {
+            if (B_search(arr, i + 1, len - 1, complement) != -1) {
+                ++count;
+            }
         }
     }
-}
-
-    return pairCount;
+    return count;
 }
